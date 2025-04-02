1ae049e088f57850215e08b02764e56e976ef9c1
Pedro Bento <>
2025-04-01 21:48:40 -0300

    .

---
+++ recipes/test/test_recipe_views.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+from django.urls import resolve, reverse
+from recipes import views
+
+class RecipeViewsTest(TestCase):
+    def test_recipe_home_views_fuction_is_correct(self):
+        view = resolve(reverse('recipes:home'))
+        self.assertIs(view.func, views.home)
+
+    def test_recipe_category_views_fuction_is_correct(self):
+        view = resolve(reverse('recipes:category',kwargs={'category_id': 1} ))
+        self.assertIs(view.func, views.category)
+
+    def test_recipe_recipe_views_fuction_is_correct(self):
+        view = resolve(reverse('recipes:recipe',kwargs={'id': 1} ))
+        self.assertIs(view.func, views.recipe)
+
+
+        
+    def test_recipe_home_view_returns_status_code_200_ok(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_home_view_loads_correct_template(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertTemplateUsed(response, 'recipes/pages/home.html')
+
+    def test_recipe_home_shows_no_recipe_found_if_no_recipe(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertIn(
+            'Não Há Receitas',
+            response.content.decode('utf-8')
+            )
