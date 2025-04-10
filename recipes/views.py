f4dfd054c0406eff828be4383c7d8b8cb8fd2357
Pedro Bento <>
2025-04-10 11:56:15 -0300

    ,

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_list_or_404,get_object_or_404
 from django.db.models import Q
 from django.http import Http404
+from django.core.paginator import Paginator
+from utils.pagination import make_pagination_range
 from recipes.models import Recipe
 
 
@@ -8,8 +10,25 @@ def home(request):
     recipes = Recipe.objects.filter(
         is_published = True
         ).order_by('-id')
+    
+    try:
+         current_page = int(request.GET.get('page', 1))
+    
+    except ValueError:
+         current_page = 1
+    
+    paginator = Paginator(recipes, 1)
+    page_obj = paginator.get_page(current_page)
+
+    pagination_range = make_pagination_range(
+        paginator.page_range,
+        4,
+        current_page
+    )
+
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range': pagination_range
     })
 
 def category(request, category_id):
