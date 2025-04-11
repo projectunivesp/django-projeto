56ac2dd823967308c8f5ec9d7ac71c9df7aef317
Pedro Bento <>
2025-04-10 23:11:13 -0300

    .

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vxp=9ojxq4wij$i)21-dcn+28k$*z%+=x+5a95s1s^4@i&4d1*'
+SECRET_KEY = os.environ.get('SECRET_KEY','INSECURE')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
 ALLOWED_HOSTS = []
 
--- recipes/views.py
+++ recipes/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render, get_list_or_404,get_object_or_404
 from django.db.models import Q
 from django.http import Http404
 from django.core.paginator import Paginator
-from utils.pagination import make_pagination_range
+from utils.pagination import make_pagination
 from recipes.models import Recipe
+
+import os
+
+PER_PAGE = int(os.environ.get('PER_PAGE', 3))
 
 
 def home(request):
@@ -11,20 +15,7 @@ def home(request):
         is_published = True
         ).order_by('-id')
     
-    try:
-         current_page = int(request.GET.get('page', 1))
-    
-    except ValueError:
-         current_page = 1
-    
-    paginator = Paginator(recipes, 1)
-    page_obj = paginator.get_page(current_page)
-
-    pagination_range = make_pagination_range(
-        paginator.page_range,
-        4,
-        current_page
-    )
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
     return render(request, 'recipes/pages/home.html', context={
         'recipes': page_obj,
@@ -39,8 +30,11 @@ def category(request, category_id):
         ).order_by('-id')
     )
 
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
+
     return render(request, 'recipes/pages/category.html', context={
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range': pagination_range,
         'title': f'{recipes.first().category.name} - Category |',
     })
 
@@ -66,9 +60,14 @@ def search(request):
     )
     recipes = recipes.order_by('-id')
     recipes = recipes.filter(is_published = True)
+
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
     
     return render(request, 'recipes/pages/search.html',
         {'page_title': f'Search for "{search_term}"',
          'search_term': search_term,
-         'recipes': recipes,
+         'recipes': page_obj,
+         'pagination_range': pagination_range,
+         'additional_url_query': f'&q={search_term}'
+         
     })
