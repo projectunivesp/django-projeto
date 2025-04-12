b3924051d6136237226a62f3b720c1dfa448a184
Pedro Bento <>
2025-04-11 21:08:36 -0300

    .

--- project/settings.py
+++ project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,3 +133,12 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MESSAGE_TAGS = {
+     constants.DEBUG: 'message-debug',
+     constants.ERROR: 'message-error',
+     constants.INFO: 'message-info',
+     constants.SUCCESS: 'message-success',
+     constants.WARNING: 'message-warning',
+ }
