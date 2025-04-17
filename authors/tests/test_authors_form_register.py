8158693a922357f4a66205cda856dca3ba163dc3
Pedro Bento <>
2025-04-16 22:54:31 -0300

    .

--- authors/tests/test_authors_form_register.py
+++ authors/tests/test_authors_form_register.py
@@ -1,5 +1,7 @@
 from authors.forms import RegisterForm
+from django.test import TestCase as DjangoTestCase
 from django.test import TestCase
+from django.urls import reverse
 from parameterized import parameterized
  
  
@@ -20,8 +22,8 @@ class AuthorRegisterFormUnitTest(TestCase):
  
     @parameterized.expand([
         ('username', (
-            'Obrigatório. 150 caracteres ou menos. '
-            'Letras, números e @/./+/-/_ apenas.')),
+        'Username must have letters, numbers or one of those @.+-_. '
+        'The length should be between 4 and 150 characters.')),
         ('email', 'The e-mail must be valid.'),
         ('password', (
             'Password must have at least one uppercase letter, '
@@ -46,3 +48,55 @@ class AuthorRegisterFormUnitTest(TestCase):
         form = RegisterForm()
         current = form[field].field.label
         self.assertEqual(current, needed)
+
+class AuthorsRegisterFormIntegrationTest(DjangoTestCase):
+    def setup(self, *args, **kwargs):
+        self.form_data = {
+            'username': 'user',
+            'first_name': 'first',
+            'last_name': 'last',
+            'email': 'email@anyemail.com',
+            'password': 'Str0ngP@ssword1',
+            'password2': 'Str0ngP@ssword1',
+        }
+
+
+        return super().setUp(*args, **kwargs)
+    
+    @parameterized.expand([
+        ('username', 'This field must not be empty'),
+        ('first_name', 'Write your first name'),
+        ('last_name', 'Write your last name'),
+        ('password', 'Password must not be empty'),
+        ('password2', 'Please, repeat your password'),
+        ('email', 'E-mail is required'),
+    ])
+
+    def test_fields_cannot_be_empty(self, field, msg):
+        self.form_data[field] = ''
+        url = reverse('authors:create')
+        response = self.client.post(url, data=self.form_data, follow=True)
+        
+        self.assertIn(msg, response.content.decode('utf-8'))
+        
+        self.assertIn(msg, response.context['form'].errors.get(field))
+    
+
+    def test_username_field_min_length_should_be_4(self):
+        self.form_data['username'] = 'joa'
+        url = reverse('authors:create')
+        response = self.client.post(url, data=self.form_data, follow=True)
+ 
+        msg = 'Username must have at least 4 characters'
+        self.assertIn(msg, response.content.decode('utf-8'))
+        self.assertIn(msg, response.context['form'].errors.get('username'))
+ 
+    def test_username_field_max_length_should_be_150(self):
+        self.form_data['username'] = 'A' * 151
+        url = reverse('authors:create')
+        response = self.client.post(url, data=self.form_data, follow=True)
+ 
+        msg = 'Username must have less than 150 characters'
+ 
+        self.assertIn(msg, response.context['form'].errors.get('username'))
+        self.assertIn(msg, response.content.decode('utf-8'))
